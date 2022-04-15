677a397ebe0c5dbeb6d917525fd8dfc09090e5a5
Tyler Prehl <62523195+tylerprehl@users.noreply.github.com>
2022-04-15 11:22:46 -0400

    Update and rename kubernetes-script-gokuins.sh to kubernetes-script-EDITED.sh

--- kubernetes-script-EDITED.sh
+++ kubernetes-script-EDITED.sh
@@ -11,23 +11,21 @@ kubectl expose deploy/registry --port=5000 --type=NodePort
 kubectl patch service registry --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30000}]'
 
 # clone GitHub repo into our Kubernetes network, cd into the folder
-git clone https://github.com/tylerprehl/CSC468-Team-Project.git
-cd ~/CSC468-Team-Project
-printf "Chill bro u got 20 seconds..."
-sleep 20 # it's a picky bitch and needs some time
+git clone https://github.com/CSC468-WCU/ram_coin.git
+printf "\nAttempting to cd into ~/ram_coin"
+cd ~/ram_coin
+sleep 30 # it's a picky bitch and needs some time
 
 # use docker-compose.images.yml file to build Kubernetes images (objects?)
 # for the GitHub components (webui, worker, hasher, generator)
 docker-compose -f docker-compose.images.yml build
-printf "Now you have a-nutha 20..."
-sleep 20
 docker-compose -f docker-compose.images.yml push
 
 # deploy and expose images
 kubectl create deployment redis --image=redis
-for SERVICE in hasher generator webui worker; do kubectl create deployment $SERVICE --image=127.0.0.1:30000/$SERVICE:v0.1; done
+for SERVICE in hasher rng webui worker; do kubectl create deployment $SERVICE --image=127.0.0.2:30000/$SERVICE:v0.1; done
 kubectl expose deployment redis --port 6379
-kubectl expose deployment generator --port 80
+kubectl expose deployment rng --port 80
 kubectl expose deployment hasher --port 80
 kubectl expose deploy/webui --type=NodePort --port=80
 
@@ -37,11 +35,13 @@ kubectl patch service webui --type='json' --patch='[{"op": "replace", "path": "/
 # setup Kubernetes dashboard
 kubectl apply -f dashboard-insecure.yaml
 kubectl apply -f socat.yaml
+kubectl get namespace
+kubectl get svc --namespace=kubernetes-dashboard
 
 # patch it to use port 30082
 kubectl patch service kubernetes-dashboard -n kubernetes-dashboard --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30082}]'
 
-# now for shits and giggles, we'll make a namespace called gokuins
-kubectl create ns gokuins
-kubectl create -f gokuins.yaml -n gokuins
-kubectl create -f gokuins-service.yaml -n gokuins
+# now for shits and giggles, we'll make a namespace called ramcoin
+kubectl create ns ramcoin
+kubectl create -f ramcoin.yaml -n ramcoin
+kubectl create -f ramcoin-service.yaml -n ramcoin
