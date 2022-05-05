e42467319bbaf3463cd1babe3e7a6510f86a1ca3
Tyler Prehl <tyler@prehl.us>
2022-05-04 23:04:08 -0400

    back on hasher.rb

--- hasher/hasher.py
+++ hasher/hasher.py
@@ -1,9 +1,6 @@
 from flask import Flask, Response
-import os
 import socket
 import time
-import hashlib
-
 
 app = Flask(__name__)
 
@@ -12,24 +9,23 @@ app.debug = os.environ.get("DEBUG", "").lower().startswith('y')
 
 hostname = socket.gethostname()
 
-ha = sha256()
 
 @app.route("/")
 def index():
-    return "HASHER running on {} \n".format(hostname)
+    return "hasher running on {}\n".format(hostname)
 
 
-@app.route("/<String:the_string")
-def hasher(the_string):
+@app.route("/<string:binary_string>")
+def hasher(binary_string):
     # Simulate a little bit of delay
     time.sleep(0.1)
-    ha.update(the_string)
-#If needed we can make the read function read the hash length and that will be the amount of bits it reads
-#Lets see how this works first.
+
+
     return Response(
-        os.read(ha.hexdigest(), 32),
+        os.read(urandom, how_many_bytes),
         content_type="application/octet-stream")
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, threaded=False)
+
