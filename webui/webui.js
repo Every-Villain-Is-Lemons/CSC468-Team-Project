bb7db3d548348d059e46690dd6d333a708d773ae
MatthewWeigand99 <70285049+MatthewWeigand99@users.noreply.github.com>
2022-05-04 20:50:07 -0400

    Update webui.js

--- webui/webui.js
+++ webui/webui.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var app = express();
-var redis = require('redis');
+//var redis = require('redis');
 
-var client = redis.createClient(6379, 'redis');
+/*var client = redis.createClient(6379, 'redis');
 client.on("error", function (err) {
     console.error("Redis error", err);
-});
+});*/
 
 app.get('/', function (req, res) {
     res.redirect('/index.html');
 });
 
-app.get('/json', function (req, res) {
+/*app.get('/json', function (req, res) {
     client.hlen('wallet', function (err, coins) {
         client.get('hashes', function (err, hashes) {
             var now = Date.now() / 1000;
@@ -22,7 +22,7 @@ app.get('/json', function (req, res) {
             });
         });
     });
-});
+});*/
 
 app.use(express.static('files'));
 
